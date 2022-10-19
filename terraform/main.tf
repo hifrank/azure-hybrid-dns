a045e96b15cfcb718e1bd749e5454893298f416d
Frank Chen <hifrankc@gmail.com>
2022-10-19 16:26:48 +0800

    fix comment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
 provider "azurerm" {
   features {}
   alias = "dns_subscription"
-  # my MSDN
+  # subscription for azure private zone resources
   subscription_id = var.dns_subscription
 }
 
 provider "azurerm" {
   features {}
   alias = "dns_policy_subscription"
-  # my MSDN
+  # subscription for azure policy resources, ex. managemnet identity.
   subscription_id = var.dns_policy_subscription
 }
 
@@ -17,7 +17,7 @@ module "dns_for_priv_endpoint" {
   providers = {
     # subscription for private DNS zone
     azurerm.dns_subscription = azurerm.dns_subscription
-    # subscription for management group
+    # subscription for azure policy resources, ex. managemnet identity.
     azurerm.dns_policy_subscription = azurerm.dns_policy_subscription
   }
   #root managemnet group
